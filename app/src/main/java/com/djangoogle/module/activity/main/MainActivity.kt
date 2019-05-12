e3399e768064b335031055e23026860803ed9eea
Djangoogle <Djangoogle@foxmail.com>
2019-05-12 21:17:17 +0800

    # 添加kotlin。

---
+++ app/src/main/java/com/djangoogle/module/activity/main/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.djangoogle.module.activity.main
+
+/**
+ * Created by Djangoogle on 2019/05/12 17:32 with Android Studio.
+ * © 2019 agesun® 安徽时旭智能科技有限公司™ All rights reserved.
+ */
+class MainActivity
