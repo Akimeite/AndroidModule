9569d983e4e64372df6aaeb97cf70cfe6c9e1732
Djangoogle <Djangoogle@foxmail.com>
2019-05-12 21:29:50 +0800

    # 删除注释。

--- app/src/main/java/com/djangoogle/module/activity/main/MainActivity.kt
+++ app/src/main/java/com/djangoogle/module/activity/main/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.djangoogle.module.activity.main
 
 /**
- * Created by Djangoogle on 2019/05/12 17:32 with Android Studio.
- * © 2019 agesun® 安徽时旭智能科技有限公司™ All rights reserved.
+ * Created by Djangoogle on 2019/05/12 21:21 with Android Studio.
  */
 class MainActivity
