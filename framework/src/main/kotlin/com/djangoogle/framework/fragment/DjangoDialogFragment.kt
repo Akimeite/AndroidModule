f9b351b58cb7520cd36427a90a7ec1456bb55634
Djangoogle <Djangoogle@foxmail.com>
2019-05-20 10:41:59 +0800

    # 优化dialog。

---
+++ framework/src/main/kotlin/com/djangoogle/framework/fragment/DjangoDialogFragment.kt
@@ -0,0 +1,48 @@
+package com.djangoogle.framework.fragment
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import com.blankj.utilcode.util.LogUtils
+import com.trello.rxlifecycle2.components.support.RxDialogFragment
+
+/**
+ * Created by Djangoogle on 2019/05/20 10:24 with Android Studio.
+ */
+open class DjangoDialogFragment : RxDialogFragment() {
+
+	companion object {
+
+		private val TAG = DjangoDialogFragment::class.simpleName
+	}
+
+	protected var mActivity: Activity? = null
+
+	override fun onAttach(context: Context?) {
+		super.onAttach(context)
+		LogUtils.dTag(TAG, "onAttach")
+		mActivity = activity
+	}
+
+	override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+		LogUtils.dTag(TAG, "onCreateDialog")
+		return super.onCreateDialog(savedInstanceState)
+	}
+
+	override fun show(manager: FragmentManager?, tag: String?) {
+		super.show(manager, tag)
+		LogUtils.dTag(TAG, "show")
+	}
+
+	override fun dismiss() {
+		super.dismiss()
+		LogUtils.dTag(TAG, "dismiss")
+	}
+
+	override fun onDetach() {
+		super.onDetach()
+		LogUtils.dTag(TAG, "onDetach")
+	}
+}
--- framework/src/main/kotlin/com/djangoogle/framework/widget/DjangoLoading.kt
+++ framework/src/main/kotlin/com/djangoogle/framework/widget/DjangoLoading.kt
@@ -2,25 +2,18 @@ package com.djangoogle.framework.widget
 
 import android.app.Dialog
 import android.os.Bundle
-import android.support.v4.app.FragmentManager
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.blankj.utilcode.util.LogUtils
 import com.djangoogle.framework.R
-import com.trello.rxlifecycle2.components.support.RxAppCompatDialogFragment
+import com.djangoogle.framework.fragment.DjangoDialogFragment
 
 /**
  * 基础等待层弹窗
  * Created by Djangoogle on 2018/10/24 09:33 with Android Studio.
  */
-class DjangoLoading : RxAppCompatDialogFragment() {
-
-	companion object {
-
-		private val TAG = DjangoLoading::class.simpleName
-	}
+class DjangoLoading : DjangoDialogFragment() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -36,22 +29,6 @@ class DjangoLoading : RxAppCompatDialogFragment() {
 		djangoLoading.setCancelable(false)
 		djangoLoading.setCanceledOnTouchOutside(false)
 		djangoLoading.setOnKeyListener { _, keyCode, _ -> KeyEvent.KEYCODE_BACK == keyCode }
-		LogUtils.dTag(TAG, "创建")
 		return djangoLoading
-	}
-
-	override fun show(manager: FragmentManager?, tag: String?) {
-		super.show(manager, tag)
-		LogUtils.dTag(TAG, "显示")
-	}
-
-	override fun dismiss() {
-		super.dismiss()
-		LogUtils.dTag(TAG, "隐藏")
-	}
-
-	override fun onDestroyView() {
-		super.onDestroyView()
-		LogUtils.dTag(TAG, "销毁")
 	}
 }
