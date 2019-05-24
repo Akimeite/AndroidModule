553522a3c2bede41368f0e71c223365eb555d6b5
Djangoogle <Djangoogle@foxmail.com>
2019-05-24 13:47:57 +0800

    # 添加服务封装。

---
+++ framework/src/main/kotlin/com/djangoogle/framework/service/DjangoService.kt
@@ -0,0 +1,38 @@
+package com.djangoogle.framework.service
+
+import android.app.Service
+import android.content.Intent
+
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+
+/**
+ * Created by Djangoogle on 2019/05/22 17:34 with Android Studio.
+ */
+abstract class DjangoService : Service() {
+
+	override fun onCreate() {
+		super.onCreate()
+		//注册EventBus
+		if (!EventBus.getDefault().isRegistered(this)) {
+			EventBus.getDefault().register(this)
+		}
+	}
+
+	override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+		return START_STICKY
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		//注销EventBus
+		if (EventBus.getDefault().isRegistered(this)) {
+			EventBus.getDefault().unregister(this)
+		}
+	}
+
+	@Subscribe(threadMode = ThreadMode.MAIN)
+	fun onBaseServiceEvent(event: Any) {
+	}
+}
