9f37d400ea84df6e4dc70a460e12ccd31dc4e0c7
Djangoogle <Djangoogle@foxmail.com>
2019-05-25 10:07:02 +0800

    # 优化代码。

--- app/src/main/java/com/djangoogle/module/application/App.kt
+++ app/src/main/java/com/djangoogle/module/application/App.kt
@@ -2,10 +2,7 @@ package com.djangoogle.module.application
 
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.djangoogle.banner.BannerEventBusIndex
-import com.djangoogle.framework.FrameworkEventBusIndex
 import com.djangoogle.framework.application.DjangoApp
-import org.greenrobot.eventbus.EventBus
 
 /**
  * Created by Djangoogle on 2019/03/27 10:53 with Android Studio.
@@ -17,12 +14,4 @@ class App : DjangoApp() {
 		MultiDex.install(this)
 	}
 
-	override fun onCreate() {
-		super.onCreate()
-		//初始化EventBus索引
-		EventBus.builder()
-			.addIndex(BannerEventBusIndex())
-			.addIndex(FrameworkEventBusIndex())
-			.installDefaultEventBus()
-	}
 }
