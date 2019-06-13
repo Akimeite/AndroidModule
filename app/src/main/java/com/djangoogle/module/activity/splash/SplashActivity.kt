e62662efb8135715efb29378982f1ca2fd69b724
Djangoogle <Djangoogle@foxmail.com>
2019-06-13 11:15:33 +0800

    # 更新VLC包。

--- app/src/main/java/com/djangoogle/module/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/djangoogle/module/activity/splash/SplashActivity.kt
@@ -2,10 +2,19 @@ package com.djangoogle.module.activity.splash
 
 import android.content.Intent
 import android.os.Build
-import android.os.Handler
+import com.blankj.utilcode.constant.PermissionConstants
+import com.blankj.utilcode.util.PermissionUtils
+import com.blankj.utilcode.util.ToastUtils
 import com.djangoogle.module.R
 import com.djangoogle.module.activity.base.BaseActivity
 import com.djangoogle.module.activity.main.MainActivity
+import com.trello.rxlifecycle3.kotlin.bindToLifecycle
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**启动页
  * Created by Djangoogle on 2019/05/13 16:44 with Android Studio.
@@ -34,30 +43,40 @@ class SplashActivity : BaseActivity() {
 	 * 申请权限
 	 */
 	private fun requestPermissions() {
-//		XXPermissions.with(this)
-//			.constantRequest()
-//			.permission(*Permission.Group.STORAGE)
-//			.request(object : OnPermission {
-//				override fun hasPermission(granted: List<String>, isAll: Boolean) {
-//					//初始化
-//					init()
-//				}
-//
-//				override fun noPermission(denied: List<String>, quick: Boolean) {
-//					val deniedArray = denied.toTypedArray()
-//					ToastUtils.showShort("权限" + Arrays.toString(deniedArray) + "被拒绝")
-//				}
-//			})
+		PermissionUtils.permission(PermissionConstants.STORAGE).callback(object : PermissionUtils.FullCallback {
+			override fun onGranted(permissionsGranted: MutableList<String>?) {
+				//初始化
+				init()
+			}
+
+			override fun onDenied(permissionsDeniedForever: MutableList<String>?, permissionsDenied: MutableList<String>?) {
+				val deniedForeverArray = permissionsDeniedForever?.toTypedArray()
+				val deniedArray = permissionsDenied?.toTypedArray()
+				ToastUtils.showShort("权限" + Arrays.toString(deniedForeverArray) + "被永久拒绝\n权限" + Arrays.toString(deniedArray) + "被拒绝")
+			}
+		}).request()
 	}
 
 	/**
 	 * 初始化
 	 */
 	private fun init() {
-		Handler().postDelayed({
-			//延迟打开轮播页
-			startActivity(Intent(mActivity, MainActivity::class.java))
-			finish()
-		}, 1000L)
+		Observable.timer(1L, TimeUnit.SECONDS, AndroidSchedulers.mainThread()).bindToLifecycle(this).subscribe(object : Observer<Long> {
+			override fun onSubscribe(d: Disposable) {}
+
+			override fun onNext(aLong: Long) {
+				//延迟打开轮播页
+				startActivity(Intent(mActivity, MainActivity::class.java))
+				finish()
+			}
+
+			override fun onError(e: Throwable) {
+				//延迟打开轮播页
+				startActivity(Intent(mActivity, MainActivity::class.java))
+				finish()
+			}
+
+			override fun onComplete() {}
+		})
 	}
 }
